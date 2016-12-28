6534086d08f79f78be47b35ec8046ce00e586a9a
Wilson Mok <Wilson@Wilsons-MacBook-Pro.local>
2016-12-28 01:02:15 -0700

    Assignment 2: 4/6 completed.  Need comments

---
+++ plot4.R
@@ -0,0 +1,25 @@
+library(dplyr)
+library(ggplot2)
+
+source("readData.R")
+
+plotfile <- "plot4.png"
+
+nei <- readNEIData()
+scc <- readSCCData()
+
+# filter to include coal combustion-related SCC
+scc_comb_coal <- filter(scc,grepl("Comb.+Coal", EI.Sector))
+
+nei_comb_coal <- filter(nei, SCC %in% scc_comb_coal$SCC)
+
+nei_year <- group_by(nei_comb_coal, year)
+total_coal_emission <- summarise(nei_year, emission = sum(Emissions))
+
+png(plotfile, width=480, height=480, units = "px")
+
+with(total_coal_emission, qplot(year, log10(emission), geom=c("point", "line"),
+                           xlab="Year", ylab="Emissions (log10)", 
+                           main="Annual coal combustion-related emission"))
+
+dev.off()
